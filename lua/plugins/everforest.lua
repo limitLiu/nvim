380e0002c0a724d50af3673b8c726e9921317280
limit <limitliu@qq.com>
2023-05-14 22:16:34 +0800

    style: everforest lua

---
+++ lua/plugins/everforest.lua
@@ -0,0 +1,9 @@
+local everforest = requirePlugin "everforest"
+if everforest then
+  everforest.setup {
+    background = "medium",
+    transparent_background_level = 0,
+    italics = true,
+    disable_italic_comments = true,
+  }
+end

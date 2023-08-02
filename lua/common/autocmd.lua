f0247a29c55f23556c092886a4efded9762f0c51
Limit Liu <limitliu@qq.com>
2023-08-02 13:56:16 +0800

    feat: go to last used hidden buffer when deleting a buffer

--- lua/common/autocmd.lua
+++ lua/common/autocmd.lua
@@ -52,3 +52,18 @@ autocmd("TextYankPost", {
   group = autoGroup,
   pattern = "*",
 })
+
+-- Go to last used hidden buffer when deleting a buffer
+autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    local api = require "nvim-tree.api"
+    if #vim.api.nvim_list_wins() == 1 and api.tree.is_tree_buf() then
+      vim.defer_fn(function()
+        api.tree.toggle { find_file = true, focus = true }
+        api.tree.toggle { find_file = true, focus = true }
+        vim.cmd "wincmd p"
+      end, 0)
+    end
+  end,
+})

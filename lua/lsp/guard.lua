76422408a6380c213baf8792829789451418b633
Limit Liu <limitliu@qq.com>
2023-09-25 00:03:26 +0800

    fix: clang-tidy exit 1

--- lua/lsp/guard.lua
+++ lua/lsp/guard.lua
@@ -1,8 +1,8 @@
 local ft = requirePlugin "guard.filetype"
 if ft then
-  ft("c"):fmt("clang-format"):lint "clang-tidy"
-  ft("lua"):fmt("stylua")
-  ft("rust"):fmt("rustfmt")
+  ft("c"):fmt "clang-format"
+  ft("lua"):fmt "stylua"
+  ft("rust"):fmt "rustfmt"
 
   require("guard").setup {
     fmt_on_save = true,

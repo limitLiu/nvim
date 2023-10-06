131ec584627096fe2d55a3db25c0d4ea81ba7722
Limit Liu <limitliu@qq.com>
2023-10-06 20:50:37 +0800

    feat: config indent blank line

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,13 +1,63 @@
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+local hooks = require "ibl.hooks"
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
 require("ibl").setup {
   exclude = {
     filetypes = {
-      "lspinfo",
-      "packer",
-      "checkhealth",
-      "help",
-      "man",
+      "startify",
       "dashboard",
-      "",
+      "dotooagenda",
+      "log",
+      "fugitive",
+      "gitcommit",
+      "packer",
+      "vimwiki",
+      "markdown",
+      "json",
+      "txt",
+      "vista",
+      "help",
+      "todoist",
+      "NvimTree",
+      "peekaboo",
+      "git",
+      "TelescopePrompt",
+      "undotree",
+      "flutterToolsOutline",
+      "", -- for all buffers without a file type
     },
+    buftypes = { "terminal", "nofile" },
+  },
+  indent = {
+    char = "",
+  },
+  scope = {
+    char = "|",
+    highlight = highlight,
+    show_start = false,
+    show_end = false,
   },
 }
+
+hooks.register(
+  hooks.type.SCOPE_HIGHLIGHT,
+  hooks.builtin.scope_highlight_from_extmark
+)

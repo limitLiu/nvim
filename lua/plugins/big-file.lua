404057ff30109fb61805f831efcf1482df21fe4b
Limit Liu <limitliu@qq.com>
2026-03-31 22:51:13 +0800

    chore: update lsp config

---
+++ lua/plugins/big-file.lua
@@ -0,0 +1,14 @@
+require("bigfile").setup {
+  filesize = 2,
+  pattern = { "*" },
+  features = {
+    "indent_blankline",
+    "illuminate",
+    "lsp",
+    "treesitter",
+    "syntax",
+    "matchparen",
+    "vimopts",
+    "filetype",
+  },
+}
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -21,7 +21,7 @@ require("conform").setup {
     },
   },
   format_on_save = {
-    timeout_ms = 500,
+    timeout_ms = 3000,
     lsp_fallback = true,
   },
 }
